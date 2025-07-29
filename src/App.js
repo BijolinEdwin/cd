0458583dd5e99e413f4d84435d833c7b9a506cba
JESUS <jesus@JESUSs-MacBook-Pro.local>
2025-07-29 15:38:27 +0530

    edit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Karunya
+          Karunya Institute of Technology and Sciences
         </a>
       </header>
     </div>
